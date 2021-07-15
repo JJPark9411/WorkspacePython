997f897a53fb99235f1150412d4099ef25904188
jjpark9411 <jjpark9411@gmail.com>
2021-07-16 02:09:45 +0900

    Add find_node_with_key method

--- class_data_structure/hash_table/HashTable.py
+++ class_data_structure/hash_table/HashTable.py
@@ -11,3 +11,16 @@ class LinkedList:
     def __init__(self):
         self.head = None # 링크드 리스트의 가장 앞 노드
         self.tail = None # 링크드 리스트의 가장 뒤 노드
+
+    def find_node_with_key(self, key):
+        """링크드 리스트에서 주어진 데이터를 갖고 있는 노드를 리턴한다. 단, 해당 노드가 없으면 None을 리턴한다."""
+        iterator = self.head # 링크드 리스트를 돌기 위해 필요한 노드 변수
+
+        while iterator is not None:
+            if iterator.key == key:
+                return iterator
+
+            iterator = iterator.next
+
+        return None
+    
