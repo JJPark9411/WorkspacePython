5caa16fa60d68203b5dd525b37bd154d7261eb31
jjpark9411 <jjpark9411@gmail.com>
2021-10-09 23:03:04 +0900

    Add sign-in method and some examples

--- class_object_oriented_programming/object/class_instance.py
+++ class_object_oriented_programming/object/class_instance.py
@@ -3,6 +3,14 @@ class User:
         # 인사 메시지 출력 메소드
         print("Hello! I'm {}.".format(some_user.name))
 
+    def sign_in(some_user, my_email, my_password):
+        # 로그인 메소드
+        if some_user.email == my_email and some_user.password == my_password:
+            print("Hello! {}.".format(some_user.name))
+        else:
+            print("Failed to sign in.")
+
+
 user1 = User()
 user2 = User()
 user3 = User()
@@ -20,5 +28,7 @@ user3.email = "choi@codeit.kr"
 user3.password = "13579"
 
 User.say_hello(user1)
-User.say_hello(user2)
-User.say_hello(user3)
+user1.say_hello() # instance의 method는 instance를 첫 번째 인자로 받는다.
+
+# user1.sign_in(user1, "kim@codeit.kr", "12345") # 에러 발생
+user1.sign_in("kim@codeit.kr", "12345")
