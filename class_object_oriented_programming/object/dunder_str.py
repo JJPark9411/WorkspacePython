c057a11100bdb423c788b23be91de8d26cb1268d
jjpark9411 <jjpark9411@gmail.com>
2021-10-09 23:27:28 +0900

    Add class variable to check the number of users

--- class_object_oriented_programming/object/dunder_str.py
+++ class_object_oriented_programming/object/dunder_str.py
@@ -1,9 +1,13 @@
 class User:
+    count = 0
+
     def __init__(self, name, email, pw):
         # 유저 인스턴스의 모든 변수를 지정해주는 메소드
         self.name = name
         self.email = email
         self.pw = pw
+
+        User.count += 1
 
     def say_hello(self):
         print("Hello! I'm {}.".format(self.name))
@@ -14,6 +18,9 @@ class User:
 
 user1 = User("Kim", "kim@codeit.kr", "12345")
 user2 = User("Lee", "lee@codeit.kr", "67890")
+user3 = User("Park", "park@codeit.kr", "67890")
 
 print(user1)
 print(user2)
+
+print(User.count)
