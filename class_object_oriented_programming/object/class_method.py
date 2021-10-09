129e3a7e251128b558a2f50364a853f69d57ef2c
jjpark9411 <jjpark9411@gmail.com>
2021-10-10 00:06:27 +0900

    Add static method is_valid_email to check which email is valid

--- class_object_oriented_programming/object/class_method.py
+++ class_object_oriented_programming/object/class_method.py
@@ -18,6 +18,10 @@ class User:
     def number_of_users(cls): # class method는 첫 번째 인자로 class를 전달받는다. 이름은 일반적으로 cls로 정한다.
         print("Number of Users: {}".format(cls.count))
 
+    @staticmethod
+    def is_valid_email(email_address):
+        return "@" in email_address
+
 
 user1 = User("Kim", "kim@codeit.kr", "12345")
 user2 = User("Lee", "lee@codeit.kr", "12346")
@@ -25,3 +29,6 @@ user3 = User("Park", "park@codeit.kr", "12347")
 
 User.number_of_users() # class method는 class로 호출하든 instance로 호출하든 첫 번째 인자로 class를 전달한다.
 user1.number_of_users() # class method는 class로 호출하든 instance로 호출하든 첫 번째 인자로 class를 전달한다.
+
+print(User.is_valid_email("test"))
+print(User.is_valid_email("test@codeit.kr"))
