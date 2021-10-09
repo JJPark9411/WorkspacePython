6768e0da8f11f250a29b31fd71f1b58da6bdb292
jjpark9411 <jjpark9411@gmail.com>
2021-10-09 22:52:33 +0900

    Add some examples for instance variables

--- class_object_oriented_programming/object/class_instance.py
+++ class_object_oriented_programming/object/class_instance.py
@@ -5,3 +5,18 @@ class User:
 user1 = User()
 user2 = User()
 user3 = User()
+
+user1.name = "Kim"
+user1.email = "kim@codeit.kr"
+user1.password = "12345"
+
+user2.name = "Park"
+user2.email = "park@codeit.kr"
+user2.password = "67890"
+
+user3.name = "Choi"
+user3.email = "choi@codeit.kr"
+user3.password = "13579"
+
+print(user1.email)
+print(user2.password)
