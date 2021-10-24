ff1df1538aea813d4dd0bddb754d9ae6bdd5df30
jjpark9411 <jjpark9411@gmail.com>
2021-10-24 17:38:20 +0900

    Add Keyboard abstract class to apply OCP

--- class_object_oriented_programming/SOLID/ocp_example1.py
+++ class_object_oriented_programming/SOLID/ocp_example1.py
@@ -1,17 +1,44 @@
-class AppleKeyboard:
-    """애플 키보드 클래스"""
+from abc import ABC, abstractmethod
 
+
+class Keyboard(ABC):
+    @abstractmethod
+    def save_input(self, content: str) -> None:
+        """키보드 인풋 저장 메소드"""
+        pass
+
+    @abstractmethod
+    def send_input(self) -> str:
+        """키보드 인풋 전송 메소드"""
+        pass
+
+
+class AppleKeyboard(Keyboard):
+    """애플 키보드 클래스"""
     def __init__(self):
         """키보드 인풋과 터치바 인풋"""
         self.keyboard_input = ""
 
-    def set_keyboard_input(self, input):
+    def save_input(self, content: str) -> None:
         """키보드 인풋 저장 메소드"""
-        self.keyboard_input = input
+        self.keyboard_input = content
 
-    def send_keyboard_input(self):
+    def send_input(self) -> str:
         """키보드 인풋 전송 메소드"""
         return self.keyboard_input
+
+
+class SamsungKeyboard(Keyboard):
+    """삼성 키보드 클래스"""
+    def __init__(self):
+        """키보드 인풋"""
+        self.user_input = ""
+
+    def save_input(self, content: str) -> None:
+        self.user_input = content
+
+    def send_input(self) -> str:
+        return self.user_input
 
 
 class KeyboardManager:
@@ -25,15 +52,18 @@ class KeyboardManager:
 
     def get_keyboard_input(self):
         """유저가 키보드로 입력한 내용을 받아오는 메소드"""
-        return self.keyboard.send_keyboard_input()
+        return self.keyboard.send_input()
 
 
 keyboard_manager = KeyboardManager()
 
 apple_keyboard = AppleKeyboard()
+samsung_keyboard = SamsungKeyboard()
 
 keyboard_manager.connect_to_keyboard(apple_keyboard)
+apple_keyboard.save_input("애플, 안녕하세요.")
+print(keyboard_manager.get_keyboard_input())
 
-apple_keyboard.set_keyboard_input("안녕하세요")
-
+keyboard_manager.connect_to_keyboard(samsung_keyboard)
+samsung_keyboard.save_input("삼성, 안녕하세요.")
 print(keyboard_manager.get_keyboard_input())
