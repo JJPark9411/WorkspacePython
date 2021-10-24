f0a596a9fd324d3dd334520d331d946d65b7b6d0
jjpark9411 <jjpark9411@gmail.com>
2021-10-24 17:59:53 +0900

    Add some comments

--- class_object_oriented_programming/SOLID/lsp_example2.py
+++ class_object_oriented_programming/SOLID/lsp_example2.py
@@ -30,6 +30,8 @@ class Rectangle:
         self._height = value if value > 0 else 1
 
 
+# 리스코프 치환 원칙을 위반한 예시
+# 정사각형은 직사각형의 행동규약을 지키기 어렵다. 따라서 정사각형 class는 직사각형 class를 상속하는 것이 바람직하지 않을 수 있다.
 class Square(Rectangle):
     def __init__(self, side):
         super().__init__(side, side)
